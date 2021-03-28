4cb89b845a5ca3c2312b1fdf3c65dcddeec93cc2
wdpm <1137299673@qq.com>
2021-03-28 20:32:34 +0800

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,60 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Mad Library',
+  tagline: 'Just make a dream or make a mad',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'mad-center', // Usually your GitHub org/user name.
+  projectName: 'mad-library', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Mad Library',
+      logo: {
+        alt: 'Mad Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/mad-center/mad-library',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'light',
+      copyright: `Copyright © ${new Date().getFullYear()} Mad library, org. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ sidebars.js
@@ -0,0 +1,24 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Docusaurus Tutorial',
+      items: [
+        'getting-started',
+        'create-a-page',
+        'create-a-document',
+        'create-a-blog-post',
+        'markdown-features',
+        'thank-you',
+      ],
+    },
+    //  add new category
+    {
+      type: 'category',
+      label: 'some thing',
+      items: [
+        'hello-foo',
+      ],
+    },
+  ],
+};
